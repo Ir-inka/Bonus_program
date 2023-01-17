5ae4d5f07a109e269f078a27cadd4b8ccf18ecb6
Irina <sailor85@inbox.ru>
2023-01-17 10:21:27 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 3000; // цена билета
         int amount = 20; // за каждые 20 рублей начисление 1 миля
-        int miles = ticket / amount; //
+        int miles = ticket / amount; 
 
         System.out.println("Количество начисленных миль:" + miles);
 
